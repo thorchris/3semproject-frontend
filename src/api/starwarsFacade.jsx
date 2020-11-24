5cdc2410f7d6dca1427844d418bd20b64946f800
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-11-24 12:12:54 +0100

    Create starwarsFacade.jsx

---
+++ src/api/starwarsFacade.jsx
@@ -0,0 +1,41 @@
+import SERVER_URL from "../util/Settings";
+
+function getChars() {
+  return fetch(SERVER_URL + "starwars/all")
+    .then(handleHttpErrors)
+    .catch((err) => {
+      if (err.status) {
+        err.fullError.then((e) => console.log(e.message));
+      } else {
+        console.log("Network error");
+      }
+    });
+}
+
+
+const apiFacade = {
+  getChars,
+};
+
+function makeOptions(method, body) {
+  var opts = {
+    method: method,
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+  };
+  if (body) {
+    opts.body = JSON.stringify(body);
+  }
+  return opts;
+}
+
+function handleHttpErrors(res) {
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, fullError: res.json() });
+  }
+  return res.json();
+}
+
+export default apiFacade;
