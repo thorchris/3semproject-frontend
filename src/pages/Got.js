bbdf711bc79559c0b74bcba655134784d5b01959
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-11-24 19:56:29 +0100

    Update Got.js

--- src/pages/Got.js
+++ src/pages/Got.js
@@ -2,7 +2,7 @@ import apiGotFacade from "../api/apiGotFacade";
 import React, { useState, useEffect } from "react";
 
 export default function Got() {
-  const [dataFromServer, setDataFromServer] = useState("");
+  const [dataFromServer, setDataFromServer] = useState([]);
 
   useEffect(() => {
     apiGotFacade.getCharacters().then((data) => setDataFromServer(data));
@@ -13,20 +13,30 @@ export default function Got() {
       <div className="row">
         <div className="col-3"></div>
         <div className="col-6 text-center">
-          <h2 className="text-center mt-5 mb-2">GOT Characters</h2>
-          <div>
-            {dataFromServer && dataFromServer.length > 0
-              ? dataFromServer.map((dataFromServer) => {
-                  return (
-                    <div key={dataFromServer.fullName}>
-                      {dataFromServer.fullName}
-                    </div>
-                  );
-                })
-              : "Loading..."}
-          </div>
-          <div className="col-3"></div>
+          <h2 className="text-center mt-5 mb-2">Game of thrones characters</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Title</th>
+                <th scope="col">Family </th>
+                <th scope="col">Points </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dataFromServer.map((m) => (
+                <tr key={m.fullName}>
+                  <td>{m.fullName}</td>
+                  <td>{m.title}</td>
+                  <td>{m.family}</td>
+                  <td>69</td>
+                  <button className="btn btn-primary">Upvote</button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
+        <div className="col-3"></div>
       </div>
     </div>
   );
