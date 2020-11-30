d9e1457a1968359c625f9e92025503b0a2792e06
thorchris <56426781+thorchris@users.noreply.github.com>
2020-11-30 10:49:33 +0100

    Search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,32 @@
+import "bootstrap/dist/css/bootstrap.css";
+import { Button, Form } from "react-bootstrap";
+import 'mdbreact/dist/css/mdb.css';
+import { MDBCol, MDBFormInline, MDBBtn } from "mdbreact";
+
 export default function Home() {
   return (
     <div className="container-fluid padding">
       <div className="row">
-        <div className="col-3"></div>
-        <div className="col-6 text-center">
-          <h2 className="mt-5">This is meant as startcode</h2>
-          <h4>How to use it:</h4>
-          <p>
-            Open your project folder in a terminal or git bash and install the
-            following node modules with these commands:
-            <li>npm install bootstrap</li>
-            <li>npm install jwt-token</li>
-            <li>npm install react-router-dom</li>
-          </p>
-          <p>
-            IMPORTANT!
-            <li>Util folder contains settings.js CHANGE THE URL to your own</li>
-          </p>
-          <p>
-            The structure: 
-            <li>components folder: contains all components for the project</li>
-            <li>pages folder: holds contains "pages" rendered</li>
-            <li>api folder: holds contains facades used to fetch data</li>
-          </p>
-          <p>
-            Your time to shine
-            <li>Change the home component to render something else than this</li>
-            <li>Add components to the components if you need more</li>
-            <li>Fecth data from the api folders facades</li>
-            <li>Implement the changes on the desired "page" and add the component to App.js</li>
-          </p>
-          <div className="col-3"></div>
+        <div className="col-2"></div>
+        <div className="col-8 text-center">
+          <h4 className="mt-5">Search for your favorite character</h4>
+          <Form>
+            <Form.Control placeholder="Search here.." />
+            <Form.Text className="text-muted">
+              Type character name, movie, tv show etc.
+            </Form.Text>
+            <MDBBtn
+              outline
+              color="primary"
+              rounded
+              size="m"
+              type="submit"
+              className="mr-auto"
+            >
+              Search
+            </MDBBtn>
+          </Form>
+          <div className="col-2"></div>
         </div>
       </div>
     </div>
