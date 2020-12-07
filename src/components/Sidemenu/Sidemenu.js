fdee4030936faf225fe121281b90a8048abb6b44
thorchris <56426781+thorchris@users.noreply.github.com>
2020-12-07 10:51:30 +0100

    emoji fun

--- src/components/Sidemenu/Sidemenu.js
+++ src/components/Sidemenu/Sidemenu.js
@@ -8,27 +8,32 @@ export default function SideMenu({ loginMsg, isLoggedIn, user }) {
     // Pass on our props
     <Menu>
           <NavLink className="menu-item" to="/">
+          <span role="img" aria-label="star-wars">🏠</span>
             Home
           </NavLink>
 
           <NavLink className="menu-item" to="/harry-potter">
+          <span role="img" aria-label="star-wars">⚡</span>
             Harry Potter
           </NavLink>
 
           <NavLink className="menu-item" to="/got">
+          <span role="img" aria-label="star-wars">⚔️</span>
             Game of Thrones
           </NavLink>
 
           <NavLink className="menu-item" to="/starwars">
+          <span role="img" aria-label="star-wars">🌠</span>
             Star Wars
           </NavLink>
 
           <NavLink className="menu-item" to="/login-out">
+          <span role="img" aria-label="star-wars">🔒</span>
             {loginMsg}
           </NavLink>
           {isLoggedIn && (
           <>
-            <span role="img" aria-label="user">&#x1f4e9; {username}</span>
+            <span role="img" aria-label="user">👨{username}</span>
           </>
         )}
     </Menu>
