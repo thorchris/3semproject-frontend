e90c2717a3ebe2edae3afb4377f6da9b713947a3
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-12-09 09:55:36 +0100

    Fecthes data to three list

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,28 @@
 import searchFacade from "../api/searchFacade";
 import React, { useState, useEffect } from "react";
+import Select from "react-select";
 
 export default function Search({ searchingForChar }) {
-  const init = [{ name: "", fullName: "" }];
+  const init = [{ name: "" }];
   const [allCharacters, setAllCharacters] = useState(init);
+  const [swCharacters, setSwCharacters] = useState(init);
+  const [gotCharacters, setGotCharacters] = useState(init);
+  const [hpCharacters, setHpCharacters] = useState([{ fullName: "" }]);
 
   const fetchData = () => {
     searchFacade
       .searchForAllChars()
-      .then((data) =>
-        setAllCharacters([
-          ...data.swList.results,
-          ...data.hpList.hpDTOList,
-          ...data.gotList.results,
-        ])
-      );
+      .then((data) => setSwCharacters([...data.swList.results]));
+    searchFacade
+      .searchForAllChars()
+      .then((data) => setGotCharacters([...data.gotList.results]));
+    searchFacade
+      .searchForAllChars()
+      .then((data) => setHpCharacters([...data.hpList.hpDTOList]));
   };
+  console.log("HP: " + hpCharacters);
+  console.log("SW: " + swCharacters);
+  console.log("GOT: " + gotCharacters);
 
   useEffect(() => {
     fetchData();
